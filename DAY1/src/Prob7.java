4eb68ca503e42a073a494ed2d5604e2951bf68fb
isha-singh1704 <isha.singh@bounteous.com>
2025-02-12 13:06:25 +0530

    add-day2p2-soln

--- DAY1/src/Prob7.java
+++ DAY1/src/Prob7.java
@@ -1,4 +1,10 @@
 public class Prob7 {
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        Prob7 prob = new Prob7();
+        System.out.println("Maximum contiguous sum: " + prob.maxSubArray(arr));
+
+    }
     public int maxSubArray(int[] arr) {
         int start=0;
         int end=0;
@@ -20,7 +26,6 @@ public class Prob7 {
 
 
         }
-        Math.min(start,end);
         return max_sofar;
 
 
---
+++ DAY2/src/Prob2.java
@@ -0,0 +1,45 @@
+public class Prob2 {
+    public static void main(String[] args) {
+        Prob2 prob = new Prob2();
+        String s = "babad";
+        System.out.println(prob.longestPalindrome(s));
+
+    }
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if (n == 0) return "";
+
+        boolean[][] dp = new boolean[n][n];
+        int start = 0;
+        int maxLength = 1;
+
+        // All substrings of length 1 are palindromes
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        // Check substrings of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+
+        // Check substrings of length > 2
+        for (int length = 3; length <= n; length++) {
+            for (int i = 0; i < n - length + 1; i++) {
+                int j = i + length - 1;
+                if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = true;
+                    start = i;
+                    maxLength = length;
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLength);
+
+    }
+}
